973653aff0014fce4e03dbcd89ace458b37e3c99
OLUBODUN IMRAN <160875068+justimran1@users.noreply.github.com>
2024-11-20 23:11:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var heading = document.querySelector("h2");//variable called heading .
+heading.addEventListener("mouseover", function (){
+  heading.style.color= "#f4d04e";
+} );
+heading.addEventListener("mouseout", function (){
+    heading.style.color = "black";
+  } );
+  
+  heading.addEventListener("touchstart", function (){
+    heading.style.color = "black";
+  } );
+
+  heading.addEventListener("touchend", function (){
+    heading.style.color = "black";
+  } );
+
